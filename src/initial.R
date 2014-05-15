89eecf43762ef09e431e5536d7927c88827e01ff
Lingbing Feng <fenglb88@gmail.com>
2014-05-15 13:54:47 +1000

    preliminary analysis

--- src/initial.R
+++ src/initial.R
@@ -2,21 +2,35 @@
 ##  setting up for modeling --------------------------------------------
 # loading required packages and data
 library(cutoff, SpatioTemporal)
-library(ggplot2, mvtsplot, reshape2)
+library(ggplot2, mvtsplot)
+library(reshape2)
 load("./data/station.rda")
 data(hqmr.data)
 data(date.month)
+
+##
 
 ## cube-transform the data and attaching date (month) tags
 hqmr.cube <- data.frame((hqmr.data[, 1:78])^(1/3))
 rownames(hqmr.cube) <- date.month
 hqmr.cube$date <- date.month
 
-m = 240 # number of months for modeling 
-s = 28 # number of stations for modeling
-subdata <- Cut(hqmr.cube)[1:m, c(1:s)]
+# impute missing values by the CUTOFF method
+data <- Cut(hqmr.cube)
+# cut the date to the recent 30 years from 1980 - 2010
+estdata <- data[(1200 - 371):1200, ]
+
+m = 360 # number of months for modeling 
+s = 26 # number of stations for modeling
+
+
+subdata <- estdata[1:m, c(1:s)]
 mdbdata <- subdata
 rm(subdata)
+
+# normally, two is the best
+SVD.cv <- SVDsmoothCV(mdbdata, 1:5)
+plot(SVD.cv)
 
 # create a indicator matrix for zeros and non-zeros
 # non-zeros are denoted by 1
@@ -40,15 +54,17 @@ covars <- data.frame(ID = colnames(mdbdata), x = xcoor, y = ycoor,
 
 
 # create ST data object
-STmdb <- createSTdata(obs, covars, n.basis = 2)
+# ST.list <- list(ind = zeroind)
+ST.list <- NULL
+STmdb <- createSTdata(obs, covars, n.basis = 2, SpatioTemporal = ST.list)
 print(STmdb)
 # fd <- calcSmoothTrends(STmdb, n.basis = 2)$trend
 # STmdb$trend <- fd
 # plot(fd$date, STmdb$trend[, 1], ylim = c(min(STmdb$trend[, 2]), max(STmdb$trend[, 2])), type = "l")
 # lines(1:m, STmdb$trend[, 2], type = "l", lty = 2, col = 2)
 
-# LUR <- list(~alt, ~alt, ~alt)
-LUR <- processLUR(STmdb, LUR.in = NULL)
+LUR <- list(~alt, ~alt, ~alt)
+# LUR <- processLUR(STmdb, LUR.in = NULL)
 # exponential covariance for the beta-fileds and no nugget effect
 cov.beta <- list(covf = "exp", nugget = FALSE)
 # cov.beta <- list(covf = "gaussian", nugget = FALSE)
@@ -61,9 +77,9 @@ locations <- list(coords = c("x","y"), long.lat = c("long","lat"))
 
 
 # create ST model object
-mdb.model <- createSTmodel(STmdb, LUR = LUR, cov.beta = cov.beta, cov.nu = cov.nu, locations = locations)
-print(mdb.model)
-# cov.beta2 <- list(covf = c("exp", "exp2", "iid"), nugget = c(FALSE, FALSE, TRUE))
+# mdb.model <- createSTmodel(STmdb, ST = "ind", LUR = LUR, cov.beta = cov.beta, cov.nu = cov.nu, locations = locations)# cov.beta2 <- list(covf = c("exp", "exp2", "iid"), nugget = c(FALSE, FALSE, TRUE))
+mdb.model <- createSTmodel(STmdb, ST = NULL, LUR = LUR, cov.beta = cov.beta, cov.nu = cov.nu, locations = locations)# cov.beta2 <- list(covf = c("exp", "exp2", "iid"), nugget = c(FALSE, FALSE, TRUE))
+
 # mdb.model2 <- updateCovf(mdb.model, cov.beta = cov.beta2)
 # print(mdb.model2)
 
